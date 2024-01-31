c992c15de7c4b4a4be8e9ba91be2d6ca92d8fa1a
Grant Deker (WSL2) <117955357+Kiddeke@users.noreply.github.com>
2024-01-30 21:28:17 -0500

    Install Vite, downgrade node.js to work on wsl, create React boilerplate

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { QPORT, mongoDBURL } from './config.js';
+import { PORT, mongoDBURL } from './config.js';
 import mongoose from 'mongoose';
 import { Book } from './models/bookModel.js';
 import booksRoute from './routes/booksRoute.js';

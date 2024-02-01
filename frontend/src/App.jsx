8c36828756dba0f7dcad41c095f2a0d09109d22e
Grant Deker (WSL2) <117955357+Kiddeke@users.noreply.github.com>
2024-01-31 19:24:20 -0500

    Add home.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import CreateBooks from './pages/CreateBooks';
+import DeleteBook from './pages/DeleteBook';
+import ShowBook from './pages/ShowBook';
+import EditBook from './pages/EditBook';
 
 const App = () => {
   return (
     <Routes>
-        <Route parth= '' element={}/>
-        <Route parth= '' element={}/>
-        <Route parth= '' element={}/>
-        <Route parth= '' element={}/>
-        <Route parth= '' element={}/>
+        <Route path= '/' element={<Home />}/>
+        <Route path= '/books/create' element={<CreateBooks />}/>
+        <Route path= '/books/delete/:id' element={<DeleteBook />}/>
+        <Route path= '/books/details/:id' element={<ShowBook />}/>
+        <Route path= '/books/edit/:id' element={<EditBook />}/>
     </Routes>
   )
 }
